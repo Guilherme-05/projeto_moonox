3d911beae2b823aa1ec5bd0027c8b277888ed981
Guilherme <guilhermemazzo9@gmail.com>
2026-05-06 16:21:34 -0300

    a

--- simplifica_nickel.py
+++ simplifica_nickel.py
@@ -3,9 +3,13 @@ import random
 simbolos = ["🎰","🎉","🎲","💎"]
 saldo = 20.0
 
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 print("= = = = = = Cassino Senai = = = = = =")
 while saldo >= 2:
-    input("\nPressione Enter para girar (Custa R$2)")
+    input("\nPressione Enter para girar (Custa R$2)\n")
+    limpar_tela()
     saldo -= 2
 
     resultado = [random.choice(simbolos) for _ in range(3)]
@@ -20,3 +24,15 @@ while saldo >= 2:
         print("Não foi dessa vez...")
 
     print(f"Saldo Atual: R$ {saldo: .2f}")
+
+    if saldo == 0.00:
+        pergunta = input("Gostaria de Colocar mais saldo? S/N\n")
+        if pergunta == "S" or pergunta == "s":
+            inserir = float(input("Qual valor gostaria de inserir?\n"))
+            saldo = saldo + inserir
+        
+        else:
+            print("\nObrigado por jogar!")
+
+
+
